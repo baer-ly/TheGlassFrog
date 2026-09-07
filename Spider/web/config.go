e52b4b20747c87abc869789afcc26bde59996a4e
Baer-ly Amiir <219930596+baer-ly@users.noreply.github.com>
2026-09-07 13:37:56 +0200

    Feat:Added a database of targets with their unique platform-specific rules

--- Spider/web/config.go
+++ Spider/web/config.go
@@ -1,1 +1,29 @@
 package web
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Platform struct {
+	Name            string `json:"name"`
+	URLTemplate     string `json:"url_template"`
+	SuccessSelector string `json:"successSelector"`
+	FailedSelector  string `json:"failedSelector"`
+}
+
+type Config struct {
+	Platforms []Platform `json:"platforms"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	if err := json.Unmarshal(file, &config); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
